033b0fda5187b0510d9f6f7f191343cefa635da2
zanzibarer <wpragobkit@gmail.com>
2017-03-17 13:20:26 -0700

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myfirstapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.myfirstapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHello;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*TextView txtHello = (TextView) findViewById(R.id.txtHello);
+        txtHello.setText("Hello James");*/
+    }
+}
